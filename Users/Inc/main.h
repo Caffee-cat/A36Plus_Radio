21cd825b35e0f63e9d8bd394b65b1fe972d7503b
Jamiexu <doxm@foxmail.com>
2024-07-05 22:07:32 +0800

    added font address and spalsh and so on

--- Users/Inc/main.h
+++ Users/Inc/main.h
@@ -17,9 +17,22 @@
 #include "task.h"
 #include "jgfx.h"
 #include "ui.h"
+#include "usart_flash.h"
 // Written by Jamiexu
+
+// Font address
+#define FLASH_SPLASH_ADDR 0x7D000                  // file: font/splash.bin, offset: 0x7d000
+
+#define FLASH_FONT_EN_8X16_ADDR 0x83050           // file: font/font_en_8x16.fon, offset: 0x85050
+#define FLASH_FONT_EN_8X16_BLOD_ADDR 0x836B0      // file: font/font_en_8x16_blod.fon, offset: 0x85690
+#define FLASH_FONT_EN_16x32_ADDR 0x83D10      // file: font/font_en_8x16_blod.fon, offset: 0x85690
+#define FLASH_FONT_CN_16X16_ADDR 0x85570 // file: font/font_cn_16x16.fon, offset: 0x85cd0
+
+
+
 
 extern uint32_t time1_current_ms;
 
 int main(void);
+
 #endif
