f01a01be27e088d016be4c3c07c8ef888459e065
Jamiexu <doxm@foxmail.com>
2024-07-05 12:24:44 +0800

    added copyright and modified target file

--- py_utils/pack_font.py
+++ py_utils/pack_font.py
@@ -1,3 +1,35 @@
+
+'''
+* @file pack_font.py
+ * @author Jamiexu (doxm@foxmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-07-04
+ *
+ * @copyright MIT License
+
+Copyright (c) 2024 (Jamiexu or Jamie793)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ *
+'''
+
 # font = [chr(i) for i in range(0x20, 0x7E)]
 # print(len(font))
 # print(''.join(font))
@@ -39,11 +71,11 @@ def merge_files(addr, target_file, files_to_merge):
 if __name__ == '__main__':
     target_file = r'output/merged_file.bin'
     files_to_merge = [
-        'font/splash.bin',
+        'font/jamiexu_splash.bin',
         'font/font_en_8x16.fon',
         'font/font_en_8x16_blod.fon',
+        'font/font_en_16x32.fon',
         'font/font_cn_16x16.fon',
-        'font/font_cn_16x16_blod.fon'
     ]
 
     merge_files(0x7D000, target_file, files_to_merge)
